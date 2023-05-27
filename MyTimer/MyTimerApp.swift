b6b5cd70c71be71824ea2f4636bb0604e6b4da34
nogu66 <ezochi66@gmail.com>
2023-05-27 13:54:55 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by YutaNoguchi on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
